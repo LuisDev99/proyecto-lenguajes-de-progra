2ef1974e8d175e7e90c666c7f821b4ae78a08876
Luis Gerardo <luisgpr99@gmail.com>
2019-05-25 23:37:52 -0600

    Se añadieron mas test cases en el TDD

--- InfijaToPostfija/Test.h
+++ InfijaToPostfija/Test.h
@@ -22,20 +22,40 @@ public:
 			testsPassed++;
 		}
 
+		if (assert("[]", "2", true)) { //Se espera que falle el test por la mala notacion infija
+			testsPassed++;
+		}
+
+		if (assert("((", "2", true)) { //Se espera que falle el test por error de parentesis incompleto
+			testsPassed++;
+		}
+
+		if (assert("[[", "2", true)) { //Se espera que falle el test por error de corchetes incompleto
+			testsPassed++;
+		}
+
+		if (assert("a", "2", true)) { //Se espera que falle el test por error de caracter ilegal en la expresion
+			testsPassed++;
+		}
+
 		if (assert("1+2*3", "7", false)) { //Se espera que el resultado sea 7
 			testsPassed++;
 		}
 
-		cout << "Cantidad de Test Pasados: " << testsPassed << " de 3" << endl;
-		
+		system("cls");
+
+		string msg = "Cantidad de Test Pasados: " + to_string(testsPassed) + " de 7";
+		Parser::printColoredMessage(msg, 3);
+
+		//cout << "Cantidad de Test Pasados: " << testsPassed << " de 7" << endl;
 	}
 
 
-	static bool assert(string input, string expectedOutput, bool expectToFail) {
+	static bool assert(string input, string expectedOutput, bool expectedToFail) {
 
 		Parser parser{ input };
 
-		if (expectToFail) {
+		if (expectedToFail) {
 
 			if (parser.didErrorOccurred())
 				return true;
